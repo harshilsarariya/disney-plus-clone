0dc631e8b7fca7be68f7aca74460169c986d6687
Harshil Sarariya <harshilprajapati9192@gmail.com>
2021-05-10 11:13:55 +0530

    ADD : Login Page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -70,6 +70,7 @@ const ImgTitle = styled.div`
   min-height: 170px;
   width: 35vw;
   min-width: 200px;
+  margin-top :60px;
   img {
     width: 100%auto;
     height: 100%;
@@ -135,4 +136,5 @@ const Description = styled.div`
   font-size: 20px;
   margin-top: 16px;
   color: rgb(249, 249, 249);
+  max-width : 760px
 `;
